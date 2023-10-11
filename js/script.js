3d1c0f19bf6f1a325c358f1730e4f5f35d03ae10
SlimWoody <125416780+SlimWoody@users.noreply.github.com>
2023-10-11 22:45:20 +0400

    Add main functionality

--- js/script.js
+++ js/script.js
@@ -3,9 +3,16 @@ const taskInput = document.querySelector('#taskInput');
 const tasksList = document.querySelector('#tasksList');
 const emptyList = document.querySelector('#emptyList');
 
-// Отслеживаем отправку задачи
-form.addEventListener('submit', addTask)
+// Добавление задач
+form.addEventListener('submit', addTask);
 
+// Удаление задач
+tasksList.addEventListener('click', deleteTask)
+
+// Задача выполнена
+tasksList.addEventListener('click', doneTask)
+
+// Функция добавления задачи
 function addTask(event) {
     // Отменем стандартное поведение при отправки формы
     event.preventDefault();
@@ -38,5 +45,28 @@ function addTask(event) {
     // Если в EmptyList более одного элемента то "Список пуст" будет не активно.
     if (tasksList.children.length > 1) {
         emptyList.classList.add('none');
+    }
+}
+
+// Функция удаления задачи 
+function deleteTask(event){
+    // c помощью target отслеживаем элемент по которому происходит клик.
+    if (event.target.dataset.action === 'delete'){
+        console.log('DELETE!!!');
+        const parenNode = event.target.closest('.list-group-item');
+        parenNode.remove();
+    }
+
+    if (tasksList.children.length === 1) {
+        emptyList.classList.remove('none');
+    }
+}
+
+// Функция выполнения задачи 
+function doneTask(event){
+    if (event.target.dataset.action === "done"){
+        const parentNode = event.target.closest('.list-group-item');
+        const taskTitle = parentNode.querySelector('.task-title'); // querySelector позволяет искать элемент по HTML 
+        taskTitle.classList.toggle('task-title--done'); //toggle добовляет и удаляет класс
     }
 }
